ad55245d1f85131b6acc43477e7c22f49247164b
Sotnik0xFF <sotnikoff1987@gmail.com>
2023-05-03 01:56:53 +0500

    Add project files.

---
+++ Module13.Task1/Program.cs
@@ -0,0 +1,50 @@
+﻿using System.Diagnostics;
+using System.Reflection;
+
+namespace Module13.Task1;
+
+public class Program
+{
+    static void Main(string[] args)
+    {
+        Console.OutputEncoding = System.Text.Encoding.UTF8;
+
+        string text = GetEmbeddedText();
+        List<char> charList = new List<char>(text);
+        LinkedList<char> charLinkedList = new LinkedList<char>(text); 
+
+        TimeSpan t1 = MeasureExecutionTime(() => charList.Insert(0, ' '));
+        TimeSpan t2 = MeasureExecutionTime(() => charLinkedList.AddFirst(' '));
+
+        Console.WriteLine($"Вставка в начало List: {t1}");
+        Console.WriteLine($"Вставка в начало LinkedList: {t2}");
+        Console.ReadKey();
+    }
+
+    public static string GetEmbeddedText()
+    {
+        Assembly assembly = Assembly.GetExecutingAssembly();
+        string text = String.Empty;
+
+        using (Stream? stream = assembly.GetManifestResourceStream("Module13.Task1.Text1.txt"))
+        {
+            if (stream != null)
+            {
+                using (StreamReader reader = new StreamReader(stream))
+                {
+                    text = reader.ReadToEnd();
+                }
+            }
+        }
+        return text;
+    }
+
+    static TimeSpan MeasureExecutionTime(Action operation)
+    {
+        Stopwatch stopwatch = new Stopwatch();
+        stopwatch.Start();
+        operation.Invoke();
+        stopwatch.Stop();
+        return stopwatch.Elapsed;
+    }
+}
---
+++ Module13.Task2/Program.cs
@@ -0,0 +1,43 @@
+﻿using Module13.Task1;
+using System.Text;
+
+namespace Module13.Task2;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        Console.OutputEncoding = System.Text.Encoding.UTF8;
+
+        var wordsStatistics = GetWordStatistics(Task1.Program.GetEmbeddedText());
+        var topPairs = wordsStatistics.OrderByDescending(pair => pair.Value).Take(10);
+        foreach (var pair in topPairs)
+        {
+            Console.WriteLine($"{pair.Key}\t[{pair.Value}]");
+        }
+        Console.ReadKey();
+    }
+
+    static Dictionary<string, int> GetWordStatistics(string text)
+    {
+        string noPunctuationText = new(text.Where(c => !char.IsPunctuation(c)).ToArray());
+        string[] words = noPunctuationText.Split(
+            separator: new char[] { ' ', '\n', '\r', '\t' },
+            options: StringSplitOptions.RemoveEmptyEntries);
+
+        Dictionary<string, int> statistics = new();
+
+        foreach (string word in words)
+        {
+            if (statistics.ContainsKey(word))
+            {
+                statistics[word]++;
+            }
+            else
+            {
+                statistics.Add(word, 1);
+            }
+        }
+        return statistics;
+    }
+}
